7d1bae35f2aecb31054cbb48dcea73b4bfb5d8fe
Vidish Appadoo <krishnaappadoo99@gmail.com>
2025-03-24 20:15:21 +0400

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://vidish-appadoo.github.io/CST3990-Individual-Projects/frontend/SignUp_Login.html', // Replace with your frontend URL
+    origin: 'https://vidish-appadoo.github.io', // Replace with your frontend URL
     credentials: true
 }));
 
