2107d2f3f62af628b95a6fb28fa6496dcfb49a10
Vidish Appadoo <krishnaappadoo99@gmail.com>
2025-03-24 19:53:17 +0400

    change origin url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500', // Replace with your frontend URL
+    origin: 'https://vidish-appadoo.github.io/CST3990-Individual-Projects/frontend/SignUp_Login.html', // Replace with your frontend URL
     credentials: true
 }));
 
